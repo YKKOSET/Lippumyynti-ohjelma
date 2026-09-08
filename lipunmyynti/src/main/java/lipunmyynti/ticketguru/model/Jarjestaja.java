98d9c77bbbfa32c0f3a9cb691ab512b55ba0e2a3
Salminsi <salminsi@hotmail.com>
2026-09-08 23:21:49 +0300

    Lisätty entity ja repository Jarjestajalle sekä Jpa ja Hibernate dependencyt.

---
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/model/Jarjestaja.java
@@ -0,0 +1,69 @@
+package lipunmyynti.ticketguru.model;
+
+import jakarta.persistence.*;
+
+@Entity
+
+public class Jarjestaja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String etunimi;
+    private String sukunimi;
+    private String sahkoposti;
+    private String puhelin;
+
+    // @OneToMany(mappedBy = "jarjestaja")
+    // private List<Tapahtuma> tapahtumat;
+
+    public Jarjestaja() {
+    }
+
+    public Jarjestaja(String etunimi, String sukunimi, String sahkoposti, String puhelin) {
+        this.etunimi = etunimi;
+        this.sukunimi = sukunimi;
+        this.sahkoposti = sahkoposti;
+        this.puhelin = puhelin;
+    }
+
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    public void setEtunimi(String etunimi) {
+        this.etunimi = etunimi;
+    }
+
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
+    public String getSahkoposti() {
+        return sahkoposti;
+    }
+
+    public void setSahkoposti(String sahkoposti) {
+        this.sahkoposti = sahkoposti;
+    }
+
+    public String getPuhelin() {
+        return puhelin;
+    }
+
+    public void setPuhelin(String puhelin) {
+        this.puhelin = puhelin;
+    }
+
+    @Override
+    public String toString() {
+        return "Jarjestaja [etunimi=" + etunimi + ", sukunimi=" + sukunimi + ", sahkoposti=" + sahkoposti + ", puhelin="
+                + puhelin + "]";
+    }
+
+}
---
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/JarjestajaRepository.java
@@ -0,0 +1,8 @@
+package lipunmyynti.ticketguru.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import lipunmyynti.ticketguru.model.Jarjestaja;
+
+public interface JarjestajaRepository extends JpaRepository<Jarjestaja, Long> {
+
+}
