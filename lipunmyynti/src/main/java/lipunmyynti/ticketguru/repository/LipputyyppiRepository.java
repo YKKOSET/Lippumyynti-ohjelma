8c5496c025e9c6b02e22a738b8a94b8fd6aad3bf
bhj630@myy.haaga-helia.fi <bhj630@myy.haaga-helia.fi>
2026-09-10 15:18:53 +0300

    lisätty lipputyyppi luokka ja repo sille

---
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/LipputyyppiRepository.java
@@ -0,0 +1,11 @@
+package lipunmyynti.ticketguru.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import lipunmyynti.ticketguru.model.Lipputyyppi;
+
+
+public interface LipputyyppiRepository extends JpaRepository<Lipputyyppi, Long> 
+{
+
+}
+
