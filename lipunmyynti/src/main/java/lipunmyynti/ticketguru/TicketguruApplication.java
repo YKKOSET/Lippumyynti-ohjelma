e4665342b8e19d4494b31088687045c83fa3fd9c
Salminsi <salminsi@hotmail.com>
2026-09-12 22:26:17 +0300

    Kovakoodattu ensimmäinen tapahtuma ja ensimmäinen järjestäjä

--- lipunmyynti/src/main/java/lipunmyynti/ticketguru/TicketguruApplication.java
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/TicketguruApplication.java
@@ -1,7 +1,14 @@
 package lipunmyynti.ticketguru;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lipunmyynti.ticketguru.model.Jarjestaja;
+import lipunmyynti.ticketguru.model.Tapahtuma;
+import lipunmyynti.ticketguru.repository.JarjestajaRepository;
+import lipunmyynti.ticketguru.repository.TapahtumaRepository;
 
 @SpringBootApplication
 public class TicketguruApplication {
@@ -9,5 +16,21 @@ public class TicketguruApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketguruApplication.class, args);
 	}
+
+	//This section will be commented out once there is a permanent database:
+	@Bean
+	public CommandLineRunner demo(TapahtumaRepository tapahtumaRepository,
+    JarjestajaRepository jarjestajaRepository) {
+    return (args) -> {
+        //Hard-coded Jarjestajat
+        jarjestajaRepository.save(new Jarjestaja("Matti", "Halminen", "matin@kukat.fi", "0401234567"));
+        //Hard-coded Tapahtumat
+        tapahtumaRepository.save(new Tapahtuma("Keikkakeidas"));
+    };
+}
+
+
+
+
 
 }
