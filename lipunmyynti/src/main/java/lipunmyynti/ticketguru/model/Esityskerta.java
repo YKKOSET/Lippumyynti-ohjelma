fa0fe7ae48876a8fbe6833c47aed4d569abbfadd
4Apila <43903887+4Apila@users.noreply.github.com>
2026-09-13 22:53:45 +0300

    Revert "41 lisaa h2 tietokanta"

--- lipunmyynti/src/main/java/lipunmyynti/ticketguru/model/Esityskerta.java
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/model/Esityskerta.java
@@ -1,69 +1,79 @@
 package lipunmyynti.ticketguru.model;
-
 import java.sql.Timestamp;
-import java.util.List;
 
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "esityskerta")
 public class Esityskerta {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
     private Paikka paikka;
-
+    
     @ManyToOne
     private Tapahtuma tapahtuma;
     private int maxOsallistujat;
     private Timestamp alkuaika;
 
-    //@OneToMany(mappedBy = "esityskerta")
-    //private List<Lippu> liput;
 
     public Esityskerta() {
     }
 
-    public Esityskerta(Paikka paikka, Tapahtuma tapahtuma, int maxOsallistujat, Timestamp alkuaika) {
+    public Esityskerta(Paikka paikka, TapahtumaId tapahtumaId, int maxOsallistujat, Timestamp alkuaika) {
         this.paikka = paikka;
-        this.tapahtuma = tapahtuma;
+        this.tapahtumaId = tapahtumaId;
         this.maxOsallistujat = maxOsallistujat;
         this.alkuaika = alkuaika;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Paikka getPaikka() {
         return paikka;
     }
 
-    public void setPaikka(Paikka paikka) {
-        this.paikka = paikka;
-    }
-
-    public Tapahtuma getTapahtuma() {
-        return tapahtuma;
-    }
-
-    public void setTapahtuma(Tapahtuma tapahtuma) {
-        this.tapahtuma = tapahtuma;
+    public TapahtumaId getTapahtumaId() {
+        return tapahtumaId;
     }
 
     public int getMaxOsallistujat() {
         return maxOsallistujat;
     }
 
-    public void setMaxOsallistujat(int maxOsallistujat) {
-        this.maxOsallistujat = maxOsallistujat;
-    }
-
     public Timestamp getAlkuaika() {
         return alkuaika;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPaikka(Paikka paikka) {
+        this.paikka = paikka;
+    }
+
+    public void setTapahtumaId(TapahtumaId tapahtumaId) {
+        this.tapahtumaId = tapahtumaId;
+    }
+
+    public void setMaxOsallistujat(int maxOsallistujat) {
+        this.maxOsallistujat = maxOsallistujat;
     }
 
     public void setAlkuaika(Timestamp alkuaika) {
         this.alkuaika = alkuaika;
     }
 
+    @Override
+    public String toString() {
+        return "Esityskerta [id=" + id + ", paikka=" + paikka + ", tapahtumaId=" + tapahtumaId + ", maxOsallistujat="
+                + maxOsallistujat + ", alkuaika=" + alkuaika + "]";
+    }
 }
+
