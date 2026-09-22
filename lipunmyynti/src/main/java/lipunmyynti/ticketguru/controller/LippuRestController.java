414ec78b068afcf11aa5a7775afa5478be0a4489
Salminsi <salminsi@hotmail.com>
2026-09-22 14:21:31 +0300

    Luotu LippuRestController

---
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/controller/LippuRestController.java
@@ -0,0 +1,27 @@
+package lipunmyynti.ticketguru.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lipunmyynti.ticketguru.repository.EsityskertaRepository;
+import lipunmyynti.ticketguru.repository.LippuRepository;
+import lipunmyynti.ticketguru.repository.LipputyyppiRepository;
+
+@RestController 
+@RequestMapping("/api/liput") // http://localhost:8080/api/liput
+
+public class LippuRestController {
+
+    //injection: repository to restcontroller
+    private final LippuRepository lippuRepository;
+    private final EsityskertaRepository esityskertaRepository;
+    private final LipputyyppiRepository lipputyyppiRepository;
+
+    public LippuRestController(LippuRepository lippuRepository, EsityskertaRepository esityskertaRepository, LipputyyppiRepository lipputyyppiRepository) {
+        this.lippuRepository = lippuRepository;
+        this.esityskertaRepository = esityskertaRepository;
+        this.lipputyyppiRepository = lipputyyppiRepository;
+    }
+
+    
+}
