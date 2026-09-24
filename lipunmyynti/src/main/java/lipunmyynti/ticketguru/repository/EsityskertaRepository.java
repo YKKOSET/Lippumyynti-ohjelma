c34d11b8b4eeb0c9fbc1ad90a4546bf2960687ef
4Apila <tituzka@hotmail.com>
2026-09-25 00:05:19 +0300

    Lisatty kovakoodatut esityskerrat

--- lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/EsityskertaRepository.java
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/EsityskertaRepository.java
@@ -1,8 +1,11 @@
 package lipunmyynti.ticketguru.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import lipunmyynti.ticketguru.model.Esityskerta;
+import lipunmyynti.ticketguru.model.Tapahtuma;
 
 public interface EsityskertaRepository extends JpaRepository<Esityskerta, Long> {
-
+    List<Tapahtuma> findByTapahtumaId(Long id);
 }
--- lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/LippuRepository.java
+++ lipunmyynti/src/main/java/lipunmyynti/ticketguru/repository/LippuRepository.java
@@ -1,8 +1,9 @@
 package lipunmyynti.ticketguru.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import lipunmyynti.ticketguru.model.Lippu;
-
 
 public interface LippuRepository extends JpaRepository<Lippu, Long> 
 {
